a8bc3f81c2cc8dbfeb22ec98deefdc44920f2354
William Heim <wh4942@students.necsd.net>
2024-07-24 11:39:12 -0400

    Added controller and data model layer to feedController

--- server/controller/feedController.js
+++ server/controller/feedController.js
@@ -11,3 +11,53 @@ exports.getAllFeedItems = (req, res) => {
     res.send(feedItems);
 
 };
+
+exports.createFeedItems = (req, res) => {
+    const { title, body, linkUrl, imageUrl } = req.body;
+    const newFeedItem = new FeedItem(currentId++, title, body, linkUrl, imageUrl);
+    feedItems.push(newFeedItem);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201).send(newFeedItem);
+};
+
+
+exports.getFeedItemsById = (req, res) => {
+    const { id } = req.params;
+    const feedItem = feedItems.find(item => item.id === parseInt(id));
+    if (feedItem) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(feedItem);
+    } else {
+        res.status(404).send({ message: 'Feed item not found' });
+    }
+};
+
+
+exports.updateFeedItems = (req, res) => {
+    const { id } = req.params;
+    const { title, body, linkUrl, imageUrl } = req.body;
+    let feedItem = feedItems.find(item => item.id === parseInt(id));
+    if (feedItem) {
+        feedItem.title = title || feedItem.title;
+        feedItem.body = body || feedItem.body;
+        feedItem.linkUrl = linkUrl || feedItem.linkUrl;
+        feedItem.imageUrl = imageUrl || feedItem.imageUrl;
+        res.setHeader('Content-Type', 'application/json');
+        res.send(feedItem);
+    } else {
+        res.status(404).send({ message: 'Feed item not found' });
+    }
+};
+
+
+exports.deleteFeedItems = (req, res) => {
+    const { id } = req.params;
+    const index = feedItems.findIndex(item => item.id === parseInt(id));
+    if (index !== -1) {
+        const deletedItem = feedItems.splice(index, 1);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(deletedItem[0]);
+    } else {
+        res.status(404).send({ message: 'Feed item not found' });
+    }
+};
